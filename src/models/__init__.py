2b946fc6aae589ee45ff072399525a14bec7d8a7
Dmytro Popovskyi <popovskiy96@gmail.com>
2024-11-07 00:08:48 +0100

    fix circular import of Note in __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,8 +3,9 @@ from models.field import Field
 from models.phone import Phone
 from models.name import Name
 from models.address import Address
+from models.note import Note
 from models.record import Record
 from models.address_book import AddressBook
-from models.note import Note
 
-__all__ = ["Birthday", "Field", "Phone", "Record", "Name", "Address", "AddressBook", "Email", "Note"]
+
+__all__ = ["Birthday", "Field", "Phone", "Record", "Name", "Address","Note", "AddressBook", "Email"]
